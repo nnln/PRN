abaf6c64668961f1f2b9106f60e526583afc687a
nnln <1017993342@qq.com>
2019-06-06 16:32:44 +0800

    second commit PRN

--- three/Polish.java
+++ three/Polish.java
@@ -8,13 +8,13 @@ public class Polish {
 		this.c=s.toCharArray();
 	}
 	
-	/* 锟斤拷缀转锟斤拷缀 */
+	/* 中缀转后缀 */
 	public String change(){
 		for(int i=0;i<c.length;i++){
-			//锟叫讹拷锟角凤拷为锟斤拷锟斤拷
+			// 判断是否为0-9的整数
 			if(c[i]>='0'&&c[i]<='9'){
 				out+=c[i];
-			//锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟�
+			// 判断是否是运算符号
 			}else if(c[i]=='+'||c[i]=='-'||c[i]=='*'||c[i]=='/'){
 				if(stack.gettop()!=null&&stack.gettop()!='('){
 				    char temp;
@@ -24,10 +24,10 @@ public class Polish {
 				    stack.push(c[i]);
 				}
 				
-			//锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟脚ｏ拷锟斤拷锟斤拷压栈锟斤拷
+			// 判断是否为"("
 			}else if(c[i]=='('){
 				stack.push(c[i]);
-			//锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟脚ｏ拷锟斤拷时锟借将栈锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷蔚锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷直锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷[锟斤拷锟斤拷锟脚碉拷锟斤拷锟斤拷锟斤拷锟斤拷锟絔锟斤拷
+			// 判断是否为")"
 			}else if(c[i]==')'){
 				while(stack.gettop()!=null&&!(stack.gettop()=='(')){
 					out+=stack.pop();
@@ -36,12 +36,13 @@ public class Polish {
 			}else{
 			}
 		}
+		// 表达式结束
 		while(!stack.isempty()){
 			out+=stack.pop();
 		}
 		return out;
 	}
-	/* 锟捷癸拷冉锟�*/
+	/* 递归比较优先级*/
 	public void recursion(char left,char right){
 	    if(!compare(left,right)){
 	        out+=stack.pop();
@@ -54,7 +55,7 @@ public class Polish {
 	        stack.push(left);
 	    }
 	}
-	/* 锟叫讹拷锟斤拷锟斤拷锟斤拷锟斤拷燃锟�锟斤拷叽蠓祷锟絫rue */
+	/* 优先级定义及比较 */
 	public boolean compare(char left,char right){
 		int a=0,b=0;
 		switch(left){
